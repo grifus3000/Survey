f3b6a499f50afccb0dc383685027ee22ecab6506
Алексей Фирсов <adfirso2@mts.ru>
2022-10-28 10:29:05 +0300

    Initial Commit

---
+++ Survey/SurveyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyApp.swift
+//  Survey
+//
+//  Created by Фирсов Алексей on 28.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SurveyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
